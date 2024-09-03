d1696ce79a3e27092000db94239f5d6b007c5042
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-09-03 14:20:47 -0600

    prueba con nuevo rol  en el front

--- src/main/java/com/joirv/app/LtiController.java
+++ src/main/java/com/joirv/app/LtiController.java
@@ -14,28 +14,28 @@ import java.util.Map;
 
 @RestController
 public class LtiController {
-//	@Autowired
-//	private LtiService ltiService;
+	@Autowired
+	private LtiService ltiService;
 
 
-	@PostMapping("/launch")
-	public ResponseEntity<Map<String,String>> hanletLtiLaunch(HttpServletRequest request ){
-		Map<String,String> parameters = new HashMap<>();
-
-		Enumeration<String> parameterNames = request.getParameterNames();
-		while (parameterNames.hasMoreElements()) {
-			String paramName = parameterNames.nextElement();
-			String paramValue = request.getParameter(paramName);
-			parameters.put(paramName, paramValue);
-		}
-
-		parameters.forEach((key, value) -> {
-			System.out.println(key + " : " + value);
-		});
-
-		return ResponseEntity.ok(parameters);
-
-	}
+//	@PostMapping("/launch")
+//	public ResponseEntity<Map<String,String>> hanletLtiLaunch(HttpServletRequest request ){
+//		Map<String,String> parameters = new HashMap<>();
+//
+//		Enumeration<String> parameterNames = request.getParameterNames();
+//		while (parameterNames.hasMoreElements()) {
+//			String paramName = parameterNames.nextElement();
+//			String paramValue = request.getParameter(paramName);
+//			parameters.put(paramName, paramValue);
+//		}
+//
+//		parameters.forEach((key, value) -> {
+//			System.out.println(key + " : " + value);
+//		});
+//
+//		return ResponseEntity.ok(parameters);
+//
+//	}
 
 //	@PostMapping("/launch")
 //	public ResponseEntity<Map<String, String>> handleLtiLaunch(HttpServletRequest request) {
@@ -64,36 +64,36 @@ public class LtiController {
 //	}
 
 
-//	@PostMapping("/launch")
-//	public void handleLtiLaunch(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//		// Procesa la solicitud POST
-//		Map<String, String> parameters = new HashMap<>();
-//		Enumeration<String> parameterNames = request.getParameterNames();
-//		while (parameterNames.hasMoreElements()) {
-//			String paramName = parameterNames.nextElement();
-//			String paramValue = request.getParameter(paramName);
-//			parameters.put(paramName, paramValue);
-//		}
-//
-//		String useRole = parameters.get("roles");
-//		if (useRole != null) {
-//			ltiService.setUseRole(useRole);
-//			System.out.println("Rol del usuario: " + useRole);
-//		} else {
-//			System.out.println("No se recibió un rol.");
-//		}
-//
-//		// Redirige al frontend en Netlify después de procesar la solicitud
-//		response.sendRedirect("https://main--melodic-twilight-f128ff.netlify.app");
-//	}
+	@PostMapping("/launch")
+	public void handleLtiLaunch(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// Procesa la solicitud POST
+		Map<String, String> parameters = new HashMap<>();
+		Enumeration<String> parameterNames = request.getParameterNames();
+		while (parameterNames.hasMoreElements()) {
+			String paramName = parameterNames.nextElement();
+			String paramValue = request.getParameter(paramName);
+			parameters.put(paramName, paramValue);
+		}
 
-//	@GetMapping("/role")
-//	public ResponseEntity<Map<String, String>> getRole() {
-//		String role = ltiService.getUseRole();
-//		Map<String, String> response = new HashMap<>();
-//		response.put("role", role);
-//		return ResponseEntity.ok(response);
-//	}
+		String useRole = parameters.get("roles");
+		if (useRole != null) {
+			ltiService.setUseRole(useRole);
+			System.out.println("Rol del usuario: " + useRole);
+		} else {
+			System.out.println("No se recibió un rol.");
+		}
+
+		// Redirige al frontend en Netlify después de procesar la solicitud
+		response.sendRedirect("https://main--melodic-twilight-f128ff.netlify.app");
+	}
+
+	@GetMapping("/role")
+	public ResponseEntity<Map<String, String>> getRole() {
+		String role = ltiService.getUseRole();
+		Map<String, String> response = new HashMap<>();
+		response.put("role", role);
+		return ResponseEntity.ok(response);
+	}
 
 }
 
