ef144796e763c9720df1f62486e79034dc77767f
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-09-03 11:08:14 -0600

    se modifico el cors para agregar a canvas

--- src/main/java/com/joirv/app/WebConfig.java
+++ src/main/java/com/joirv/app/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		        .allowedOrigins("https://main--melodic-twilight-f128ff.netlify.app/") // Cambia esto si tu frontend está en otro puerto o dominio
-		        .allowedMethods("*")
-		        .allowedHeaders("*");
+		        .allowedOrigins("https://link4campus.instructure.com", "https://main--melodic-twilight-f128ff.netlify.app/") // Cambia esto si tu frontend está en otro puerto o dominio
+		        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+		        .allowedHeaders("*")
+		        .allowCredentials(true);
 	}
 }
